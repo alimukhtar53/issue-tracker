198beeafc65b1cab816ced5099fd8ecc1ce94c45
alimukhtar53 <alimukhtar52@gmail.com>
2023-11-03 19:17:00 +0500

    Display issues & build issue status badge

---
+++ app/components/Badge.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Badge as BadgeComponent } from "@radix-ui/themes";
+import { Status } from "@prisma/client";
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "violet" | "green" }
+> = {
+  OPEN: { label: "Open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "violet" },
+  CLOSED: { label: "Closed", color: "green" },
+};
+
+const Badge = ({ status }: { status: Status }) => {
+  return (
+    <BadgeComponent color={statusMap[status].color}>
+      {statusMap[status].label}
+    </BadgeComponent>
+  );
+};
+
+export default Badge;
